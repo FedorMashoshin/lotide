d0669dacba21bfe34b025a40585e6231e20e379e
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-07-21 03:10:03 +0000

    Added assert to my file

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(String.fromCodePoint(0x1F7E2) + `Assertion Passed: [${actual}] === [${expected}]`);
+  }  else  {
+    console.log(String.fromCodePoint(0x1F534) + ` Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
