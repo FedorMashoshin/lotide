c104bf6e2c3c9eca129804b89ecd305d0dc7197b
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-07-22 19:24:24 +0000

    Changed names of variables

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,16 @@
-function assertArraysEqual(arr1, arr2) {
-const fail = `${String.fromCodePoint(0x1F534)} Assertion Failed: [${arr1}] !== [${arr2}] ${String.fromCodePoint(0x1F534)} `;
-const pass = `${String.fromCodePoint(0x1F7E2)} Assertion Passed: [${arr1}] === [${arr2}] ${String.fromCodePoint(0x1F7E2)} `;
+function assertArraysEqual(actual, expected) {
+const fail = `${String.fromCodePoint(0x1F534)} Assertion Failed: [${actual}] !== [${expected}] ${String.fromCodePoint(0x1F534)} `;
+const pass = `${String.fromCodePoint(0x1F7E2)} Assertion Passed: [${actual}] === [${expected}] ${String.fromCodePoint(0x1F7E2)} `;
 
-    if (arr1.length != arr2.length) {
+    if (actual.length != expected.length) {
         return fail;
     }
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] != arr2[i]) {
+    for (let i = 0; i < actual.length; i++) {
+        if (actual[i] != expected[i]) {
             return fail;
         }
     }
     return pass;
 }
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); 
