69fac6e7e6363ed238ffbdca03169382e3a193a2
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-07-22 20:51:37 +0000

    Some changes in without file

--- without.js
+++ without.js
@@ -8,8 +8,6 @@ function without(arr1, arr2) {
     }
     return arr1;
 }
-console.log(without([1, 2, 3, 4, 1, 5, 6], [1, 2, 3]));
-console.log(without(["1", "2", "3"], [1, 2, "3"]));
 
 function assertArraysEqual(arr1, arr2) {
     const fail = `${String.fromCodePoint(0x1F534)} Assertion Failed: [${arr1}] !== [${arr2}] ${String.fromCodePoint(0x1F534)} `;
