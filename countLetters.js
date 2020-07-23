965d69f6c5abd8c9cf150dd92293966e36c569f1
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-07-23 03:10:30 +0000

    Deleted useless code

--- countLetters.js
+++ countLetters.js
@@ -1,16 +1,6 @@
-const green = String.fromCodePoint(0x1F7E2);
-const red = String.fromCodePoint(0x1F534);
-const assertEqual = function (actual, expected) {
-    if (actual === expected) {
-        console.log(` ${green} Assertion Passed: [${actual}] === [${expected}] ${green}`);
-    } else {
-        console.log(` ${red} Assertion Failed: [${actual}] !== [${expected}] ${red}`);
-    }
-};
-
 const countLetters = function (string) {
     const result = {};
-    string = string.replace(/\s/g, '');;
+    string = string.replace(/\s/g, '');
     for (letters of string) {
         if(result[letters]){
             result[letters] += 1
