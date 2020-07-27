a30e19c38ce549e4b90c436e08bb1788799342fa
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-07-27 21:13:43 +0000

    changed the file with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,16 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert ;
 const middle = require('../middle');
 
-console.log(assertArraysEqual(middle([18,23,45]),[23]));
+describe("#head", () => {
+    it("returns 23 for [18 ,23 ,45]", () => {
+      assert.deepEqual(middle([18, 23, 45]),[ 23 ]);
+    });
+
+    it("returns [12, 24] for [18 , 12, 24 ,45]", () => {
+        assert.deepEqual(middle([18, 12, 24, 45]) ,[12, 24]);
+      });
+
+      it("returns [] for [18 ,23]", () => {
+        assert.deepEqual(middle([18, 23]),[ ]);
+      });
+  });
