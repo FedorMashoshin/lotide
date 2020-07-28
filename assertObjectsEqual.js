3db4886f853d8e7ed02ba65c7c34569555fbbe42
Fedor_Mashoshin <fmashoshin@gmail.com>
2020-07-28 02:38:27 +0000

    Replaced emojis

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,38 +1,32 @@
-const green = String.fromCodePoint(0x1F7E2);
-const red = String.fromCodePoint(0x1F534);
-
 const assertObjectsEqual = function (object1, object2) {
     const inspect = require('util').inspect;
-    // ALERT messages (pass or fail)
-    const passMsg = ` ${green} Assertion Passed: [${inspect(object1)}] !== [${inspect(object2)}] ${green}` 
-    const failMsg = ` ${red} Assertion Failed: [${inspect(object1)}] !== [${inspect(object2)}] ${red}`
     // If not even objects return fail!
     let keys1 = Object.keys(object1);
     if (Object.keys(object1).length !== Object.keys(object2).length) {
-        return failMsg;
+        return ` 🔴🔴 Assertion Failed: [${inspect(object1)}] !== [${inspect(object2)}] 🔴🔴`;
     }
     for (key of keys1) {
         //Checking if we have 2 arrays
         if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
             //Comparing length of our arrays
             if (object1[key].length != object2[key].length) {
-                return failMsg;
+                return ` 🔴🔴 Assertion Failed: [${inspect(object1)}] !== [${inspect(object2)}] 🔴🔴`;
             }
              //Comparing values of our arrays
             for (let i = 0; i < object1[key].length; i++) {
                 if (object1[key][i] != object2[key][i]) {
-                    return failMsg;
+                    return ` 🔴🔴 Assertion Failed: [${inspect(object1)}] !== [${inspect(object2)}] 🔴🔴`;
                 }
             }
             // If our arrays are totally equal we pass 
-            return passMsg;
+            return ` 🟢🟢 Assertion Passed: [${inspect(object1)}] !== [${inspect(object2)}] 🟢🟢` ;
         }
         //If we have object --> compare values
         if (object1[key] !== object2[key]) {
-            return failMsg;
+            return ` 🔴🔴 Assertion Failed: [${inspect(object1)}] !== [${inspect(object2)}] 🔴🔴`;
         }
     }
-    return passMsg;
+    return ` 🟢🟢 Assertion Passed: [${inspect(object1)}] !== [${inspect(object2)}] 🟢🟢` ;
 }
 
 // <--------------------- TESTS ---------------------> \\
